72d8430fa9c107fcad4105c03e0005247bcc391a
mars0i <mabrams@uab.edu>
2016-12-14 13:54:27 -0600

    added defrecord in main ns, wrapped in do

--- src/namespaceexperiment/core.clj
+++ src/namespaceexperiment/core.clj
@@ -1,25 +1,33 @@
 (ns namespaceexperiment.core)
 
-(defn foo [] "The Mighty Foo!")
+;; other stuff here
 
-(ns namespaceexperiment.data)
+(do 
+  (ns namespaceexperiment.data)
 
-(defrecord Data [x y])
+  (defrecord Data [x y])
 
-(ns namespaceexperiment.core
-  (:require [namespaceexperiment.data :as d]))
+  (ns namespaceexperiment.core
+    (:require [namespaceexperiment.data :as d]))
   ;(:import [namespaceexperiment.data Data]))
 
-;(require [namespaceexperiment.data :as d])
-(import [namespaceexperiment.data Data])
+  ;(require [namespaceexperiment.data :as d])
+  (import [namespaceexperiment.data Data])
 
-(println (foo))
+  (defrecord Foo [x])
 
-(defn baz [x y] (d/->Data x y))
-(def bazdata (baz 5 7))
-(println bazdata)
+  (defn baz [x y] (d/->Data x y))
+  (def bazdata (baz 5 7))
+  (println bazdata)
 
-;(defn bar [x y] (namespaceexperiment.data/Data. x y))
-(defn bar [x y] (d/Data. x y))
-(def bardata (bar 5 7))
-(println bardata)
+  ;(defn bar [x y] (namespaceexperiment.data/Data. x y))
+  (defn bar [x y] (d/Data. x y))
+  (def bardata (bar 5 9))
+  (println bardata)
+
+  (defn foo [x] (Foo. x))
+  (println (foo 22))
+
+  (defn fooz [x] (->Foo x))
+  (println (fooz 42))
+)
